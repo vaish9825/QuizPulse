8f4da2b15d76806e19c83d3ab5a2533d33fa9df7
Vaishnavi Waghmare <your_github_email@example.com>
2026-07-06 20:04:39 +0530

    install core frontend libraries

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,7 @@
+import AppRouter from "./app/router/AppRouter";
+
 function App() {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-slate-950">
-      <h1 className="text-6xl font-bold text-indigo-500">
-        QuizPulse 🚀
-      </h1>
-    </div>
-  );
+  return <AppRouter />;
 }
 
 export default App;
---
+++ client/src/pages/HomePage.tsx
@@ -0,0 +1,11 @@
+const HomePage = () => {
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-slate-950">
+      <h1 className="text-6xl font-bold text-indigo-500">
+        QuizPulse 🚀
+      </h1>
+    </main>
+  );
+};
+
+export default HomePage;
